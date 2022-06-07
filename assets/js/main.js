a9ed295160b171d98c2dffc21659c00d09584d7a
JD Porterfield <jdrocks94@gmail.com>
2022-06-06 19:39:11 -0700

    No Ticket : Remove browser.min.js as it appears unnecessary

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,21 @@
 const BP_MD_OR_LESS = 'xxs xs sm md';
 const BP_LG_OR_MORE = 'lg xl xxl';
 const BP_ENTER = 'enter';
+const PARALLAX_FACTOR = 20; /* higher is more intense parallax */
 
 function windowScrollToTop() {
 	return (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-}
+};
 
 function bgImageIsWebp() {
 	var headerElement = document.querySelector('#header');
 	var view = headerElement.ownerDocument.defaultView;
 	return view.getComputedStyle(headerElement, null).backgroundImage.includes("webp");
-}
+};
 
 var header = document.querySelector('#header');
 var footer = document.querySelector('#footer');
 var main = document.querySelector('#main');
-var parallaxSettings = {
-	enabled: browser.name != 'ie' && !browser.mobile,
-	parallaxFactor: 20 //highter is more intense parallax
-};
 
 Breakpoints({
 	xs: {
@@ -43,21 +40,17 @@ Breakpoints({
 	}
 });
 
-if (browser.mobile) {
-	document.body.addClass('is-touch');
-}
-
-// Footer.
+/* Footer */
 Breakpoints.on(BP_MD_OR_LESS, BP_ENTER, function() {
 	if (main.parentNode) {
 		main.parentNode.insertBefore(footer, main.nextSibling);
-	}
+	};
 	if (!bgImageIsWebp()) {
 		var bgArray = ["bg.webp", "bg2.webp", "bg3.webp", "bg4.webp"];
 		var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
 		header.style.backgroundImage = "url('images/overlay.webp'), url('images/" + bg + "')";
 		footer.style.backgroundImage = "url('images/overlay.webp'), url('images/" + bg + "')";
-	}
+	};
 });
 
 Breakpoints.on(BP_LG_OR_MORE, BP_ENTER, function() {
@@ -66,29 +59,22 @@ Breakpoints.on(BP_LG_OR_MORE, BP_ENTER, function() {
 		var bgArray = ["bg.webp", "bg2.webp", "bg3.webp", "bg4.webp"];
 		var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
 		header.style.backgroundImage = "url('images/overlay.webp'), url('images/" + bg + "')";
-	}
+	};
 	footer.style.backgroundImage = "none";
 });
 
-// Header.
+/* Header with Parallax Background */
+function updateParallaxPosition() {
+	header.style.backgroundPosition = 'left ' + (-1 * (parseInt(windowScrollToTop()) / PARALLAX_FACTOR)) + 'px';
+};
 
-// Parallax background.
+Breakpoints.on(BP_MD_OR_LESS, BP_ENTER, function() {
+	window.removeEventListener('scroll', updateParallaxPosition);
+	header.style.backgroundPosition = '';
 
-if (parallaxSettings.enabled) {
+});
 
-	function updateParallaxPosition() {
-		header.style.backgroundPosition = 'left ' + (-1 * (parseInt(windowScrollToTop()) / parallaxSettings.parallaxFactor)) + 'px';
-	}
-
-	Breakpoints.on(BP_MD_OR_LESS, BP_ENTER, function() {
-		window.removeEventListener('scroll', updateParallaxPosition);
-		header.style.backgroundPosition = '';
-
-	});
-
-	Breakpoints.on(BP_LG_OR_MORE, BP_ENTER, function() {
-		header.style.backgroundPosition = 'left 0px';
-		window.addEventListener('scroll', updateParallaxPosition);
-	});
-
-}
+Breakpoints.on(BP_LG_OR_MORE, BP_ENTER, function() {
+	header.style.backgroundPosition = 'left 0px';
+	window.addEventListener('scroll', updateParallaxPosition);
+});
